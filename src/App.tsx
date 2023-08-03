a8d6e812529c183d9a36af122a0da5b693fd4e6c
neodylan <neodylan@foxmail.com>
2023-08-03 12:14:40 +0800

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,29 +6,30 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const total1 = () => {
+    console.log('total1...')
+    const list = [1, 2, 3, 4]
+    return list.reduce((prev, curr) => {
+      return prev + curr
+    }, 0)
+  }
+  const total2 = useMemo(() => {
+    console.log('total2...')
+    const list = [1, 2, 3, 4]
+    return list.reduce((prev, curr) => {
+      return prev + curr
+    }, 0)
+  }, [])
+  const handleClick = () => {
+    setCount(count + 1)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <p>{count}</p>
+      <button onClick={handleClick}>按钮</button>
+      <p>total1: {total1()}</p>
+      <p>total2: {total2}</p>
+    </div>
   )
 }
 
