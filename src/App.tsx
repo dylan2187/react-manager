7a79a100acd034c6e3a3724585f1252edabce6c0
neodylan <neodylan@foxmail.com>
2023-08-03 10:55:52 +0800

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [name, setName] = useState('dylan')
+  const isAdmin = true
+  const [list, setList] = useState(['1', '2', '3', '4'])
+  const [count, setCount] = useState(1)
+  const h1 = <h1>title</h1>
 
+  const handleClick = () => {
+    setName('wangkai')
+    // setList([...list, 2])
+  }
+  const handleList = () => {
+    setList([...list, 'tom'])
+  }
+  const handleCount = () => {
+    setInterval(() => {
+      console.log('11')
+      setCount(count + 1)
+    }, 1000)
+  }
   return (
-    <>
+    <div className='App'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <input type='text' value={name} onChange={setName('wangkai')} /> */}
+        <button onClick={handleClick}>按钮</button>
+        <button onClick={handleList}>更新列表</button>
+        <button onClick={handleCount}>异步修改count</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <span>{name}</span>
+      {h1}
+      {isAdmin ? '管理员' : '游客'}
+      {list.map(item => {
+        return <h2 key={item}>{item}</h2>
+      })}
+      <h2>count: {count}</h2>
+    </div>
   )
 }
 
