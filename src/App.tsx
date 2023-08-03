8598a1804be635016a3608f2c745fe70d6025f04
neodylan <neodylan@foxmail.com>
2023-08-03 16:03:11 +0800

    memo添加依赖值

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // 单纯函数
   const total1 = () => {
     console.log('total1...')
     const list = [1, 2, 3, 4]
@@ -13,13 +14,17 @@ function App() {
       return prev + curr
     }, 0)
   }
+  // 使用useMemo缓存值
   const total2 = useMemo(() => {
+    // useMemo缓存了一个值，不会重复更新
     console.log('total2...')
     const list = [1, 2, 3, 4]
     return list.reduce((prev, curr) => {
       return prev + curr
     }, 0)
-  }, [])
+  }, [count])
+
+  // 使用useCallback缓存函数
   const handleClick = () => {
     setCount(count + 1)
   }
@@ -29,8 +34,12 @@ function App() {
       <button onClick={handleClick}>按钮</button>
       <p>total1: {total1()}</p>
       <p>total2: {total2}</p>
+      <Child />
     </div>
   )
+}
+const Child = () => {
+  return <div>我是子节点</div>
 }
 
 export default App
